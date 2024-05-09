3ac1dab20966a9edd439a0a068bf85e39a1c6bf1
savyajajimoggala <savyajajimoggala@gmail.com>
2024-05-09 15:17:37 +0530

    added comments for vowels problem

---
+++ vowels/main.js
@@ -0,0 +1,8 @@
+/*
+Problem: Finding vowels in a given string
+Solution: need to identify a,e,i,o,u
+Step1 : creating function with name findVowels
+Step2: return final string with vowels
+Step3: add a for loop on the string
+Step4: equate it wth charcode of a(97),e,i,o,u 
+*/
